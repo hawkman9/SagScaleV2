4696224455c96aa7d90dd2890030064232b5ba93
Ben Hawk <benjaminhawkins9@gmail.com>
2017-12-18 22:04:27 -0600

    Fix the gotoSuspensionHistory and gotoCalSag.
    There were methods I deleted because I thought they were extra code and
    that goto was reserved language.

--- app/src/main/java/com/example/benja/sagscalev2/MainActivity1.java
+++ app/src/main/java/com/example/benja/sagscalev2/MainActivity1.java
@@ -6,9 +6,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
-import android.widget.TextView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
 
 public class MainActivity1 extends AppCompatActivity {
 
@@ -28,15 +28,16 @@ public class MainActivity1 extends AppCompatActivity {
             }
         });
 
-        Button button1 = (Button) findViewById(R.id.button1);
+        Button button1 = (Button)findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {goToCalSag();}
 
         });
 
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 goToSuspensionHistory();
@@ -44,5 +45,17 @@ public class MainActivity1 extends AppCompatActivity {
 
         });
 
+    }
+
+    private void goToCalSag(){
+
+        Intent intent = new Intent(this, CalSag.class);
+        startActivity(intent);
+    }
+
+    private void goToSuspensionHistory(){
+
+        Intent intent = new Intent(this, SuspensionHistory.class);
+        startActivity(intent);
     }
 }
